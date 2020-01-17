610acca8059e0527bfa7ca0679feb4b8c9dcc715
Flatiron School <email.the.anderson@gmail.com>
2020-01-17 17:00:22 -0500

    still learning

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-function sayHello(name) {
-    console.log(`Hello ${name}`)
-}
 
-// sayHello('Anderson')
-console.log(module)
+//This is how you would call the log function from logger if you were exporting logger as an object
+// const logger = require('./logger')
+
+// logger.log('message')
+
+const log = require('./logger')
+log('message')
