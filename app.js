4ca8bb54d689a844cf5449120e88bc6ead13d36f
Flatiron School <email.the.anderson@gmail.com>
2020-02-18 21:27:33 -0500

    module 14 down

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-
-//This is how you would call the log function from logger if you were exporting logger as an object
-// const logger = require('./logger')
-
-// logger.log('message')
-
-const log = require('./logger')
-log('message')
+const logger = require('./logger')
+logger('message')
