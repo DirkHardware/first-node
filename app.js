04e265ca3106de28909c7da153a015603bd3bac8
Flatiron School <email.the.anderson@gmail.com>
2020-02-24 20:17:10 -0500

    flurp

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const http = require('http');
 
-const server = http.createServer();
-
-server.on('connection', (socket) => {
-    console.log('New connection...')
+const server = http.createServer((req, res) =>{
+    if (req.url === '/') {
+        res.write('Hello World!')
+        res.end();
+    }
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1,2,3]))
+        res.end();
+    }
 });
+
+// server.on('connection', (socket) => {
+//     console.log('New connection...')
+// });
+//flurp 
 
 server.listen(3000);
 
